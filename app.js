30dffc11e45a90decb771f0dab95a1d881f2efba
Paramveer <paramveermarwahafc@gmail.com>
2025-08-10 13:42:19 +0200

    feat: added request handler for jobs

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 import express from "express";
+import { jobRouter } from "./routes/jobRouter.js";
 
 export const app = express();
 
 app.use(express.json());
+
+app.use("/api/v1/jobs", jobRouter);
