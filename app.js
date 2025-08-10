5ba2e09055b4f368d6b5560afb2e7acc19e02e82
Paramveer <paramveermarwahafc@gmail.com>
2025-08-10 15:21:41 +0200

    feature: added middlewares to handle unknown routes and to catch custom errors

--- app.js
+++ app.js
@@ -1,5 +1,11 @@
 import express from "express";
+import errorHandlerMiddleware from "./middleware/errorHandlerMiddleware.js";
+import notFound from "./middleware/notFound.js";
 
 export const app = express();
 
 app.use(express.json());
+
+app.use(notFound);
+
+app.use(errorHandlerMiddleware);
