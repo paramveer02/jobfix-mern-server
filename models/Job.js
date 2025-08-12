82970bd8a39a656db309809e0ad58e5f1ccf2a61
Paramveer <paramveermarwahafc@gmail.com>
2025-08-12 09:54:43 +0200

    fix: set export to default

--- models/Job.js
+++ models/Job.js
@@ -34,4 +34,6 @@ const jobSchema = new mongoose.Schema(
   { timestamps: true }
 );
 
-export const Job = mongoose.model("Job", jobSchema);
+const Job = mongoose.model("Job", jobSchema);
+
+export default Job;
