bbfb21df08b869da6555b636a41d8732a83c912d
Paramveer <paramveermarwahafc@gmail.com>
2025-08-14 16:59:25 +0200

    feature: cloudinary configuration

--- server.js
+++ server.js
@@ -2,10 +2,17 @@ import dotenv from "dotenv";
 import "./config/connectionDB.js";
 import { app } from "./app.js";
 import morgan from "morgan";
+import cloudinary from "cloudinary";
 
 dotenv.config();
 
 const PORT = process.env.PORT;
+
+cloudinary.config({
+  cloud_name: process.env.CLOUD_NAME,
+  api_key: process.env.CLOUD_API_KEY,
+  api_secret: process.env.CLOUD_API_SECRET,
+});
 
 if (process.env.NODE_ENV === "development") {
   app.use(morgan("dev"));
