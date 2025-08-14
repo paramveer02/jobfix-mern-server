aa65b1a8a73874e60210418e39b4bca6a23dc966
Paramveer <paramveermarwahafc@gmail.com>
2025-08-14 17:04:37 +0200

    feature: persist user profile picture avatar

--- controllers/userController.js
+++ controllers/userController.js
@@ -2,6 +2,8 @@ import { StatusCodes } from "http-status-codes";
 import Job from "../models/Job.js";
 import User from "../models/User.js";
 import { NotFoundError } from "../errors/customErrors.js";
+import cloudinary from "cloudinary";
+import { promises as fs } from "fs";
 
 export const getCurrentUser = async (req, res) => {
   const user = await User.findOne({ _id: req.user.id });
@@ -10,13 +12,29 @@ export const getCurrentUser = async (req, res) => {
 };
 
 export const updateCurrentUser = async (req, res) => {
-  const { name, lastName, email, location } = req.body;
+  const newUser = { ...req.body };
+  let oldAvatarPublicId;
 
-  const updatedUser = await User.findByIdAndUpdate(
-    req.user.id,
-    { name, lastName, email, location },
-    { new: true, runValidators: true }
-  );
+  if (req.file) {
+    // Get the current user to fetch old avatar public id
+    const currentUser = await User.findById(req.user.id);
+    oldAvatarPublicId = currentUser?.avatarPublicId;
+
+    const response = await cloudinary.v2.uploader.upload(req.file.path);
+    await fs.unlink(req.file.path);
+    newUser.avatar = response.secure_url;
+    newUser.avatarPublicId = response.public_id;
+  }
+
+  const updatedUser = await User.findByIdAndUpdate(req.user.id, newUser, {
+    new: true,
+    runValidators: true,
+  });
+
+  // Only delete the old avatar if a new one was uploaded and there was an old avatar
+  if (req.file && oldAvatarPublicId) {
+    await cloudinary.v2.uploader.destroy(oldAvatarPublicId);
+  }
 
   res.status(StatusCodes.OK).json({ user: updatedUser });
 };
