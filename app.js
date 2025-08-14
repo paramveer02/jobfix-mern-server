9884d0a59c57006a890a1b9fa02c409e02c019a0
Paramveer <paramveermarwahafc@gmail.com>
2025-08-14 16:58:20 +0200

    feature: middleware to parse static files

--- app.js
+++ app.js
@@ -3,16 +3,21 @@ import errorHandlerMiddleware from "./middleware/errorHandlerMiddleware.js";
 import notFound from "./middleware/notFound.js";
 import cookieParser from "cookie-parser";
 import cors from "cors";
+import { fileURLToPath } from "url";
+import { dirname } from "path";
+import path from "path";
 
 import { authRouter } from "./routes/authRouter.js";
 import { userRouter } from "./routes/userRouter.js";
 import { jobRouter } from "./routes/jobRouter.js";
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
+
 export const app = express();
 
 app.use(express.json());
-
 app.use(cookieParser());
+app.use(express.static(path.resolve(__dirname, "./public")));
 
 app.use(
   cors({
