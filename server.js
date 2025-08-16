3baf4b643ae1a73c505acecda784eb101c142800
Paramveer <paramveermarwahafc@gmail.com>
2025-08-16 14:28:19 +0200

    fix: minor refactoring and ordering imports

--- server.js
+++ server.js
@@ -1,23 +1,23 @@
 import dotenv from "dotenv";
+dotenv.config();
+
 import "./config/connectionDB.js";
 import { app } from "./app.js";
 import morgan from "morgan";
 import cloudinary from "cloudinary";
 
-dotenv.config();
+const PORT = process.env.PORT || 5100;
 
-const PORT = process.env.PORT;
+if (process.env.NODE_ENV !== "production") {
+  app.use(morgan("dev"));
+}
 
-cloudinary.config({
+cloudinary.v2.config({
   cloud_name: process.env.CLOUD_NAME,
   api_key: process.env.CLOUD_API_KEY,
   api_secret: process.env.CLOUD_API_SECRET,
 });
 
-if (process.env.NODE_ENV === "development") {
-  app.use(morgan("dev"));
-}
-
 app.listen(PORT, () => {
-  console.log(`App is listening on port ${PORT}`);
+  console.log(`Server listening on ${PORT} (${process.env.NODE_ENV})`);
 });
