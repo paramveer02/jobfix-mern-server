93622dd101c533230a1535eb11cf3338fbeb464d
Paramveer <paramveermarwahafc@gmail.com>
2025-08-16 16:25:20 +0200

    fix: provide stored cookie on all routes

--- utils/createSendToken.js
+++ utils/createSendToken.js
@@ -1,24 +1,23 @@
+// utils/createSendToken.js
 import jwt from "jsonwebtoken";
 
-const signToken = (userId) => {
-  return jwt.sign({ id: userId }, process.env.JWT_SECRET, {
+const signToken = (userId) =>
+  jwt.sign({ userId }, process.env.JWT_SECRET, {
     expiresIn: process.env.JWT_EXPIRES_IN,
   });
-};
 
-export const createSendToken = (user, statusCode, res) => {
+export default function createSendToken(user, statusCode, res) {
   const token = signToken(user._id);
-
-  const oneDay = 24 * 60 * 60 * 1000;
+  const isProd = process.env.NODE_ENV === "production";
 
   res.cookie("token", token, {
     httpOnly: true,
-    expires: new Date(Date.now() + oneDay),
+    secure: isProd,
+    sameSite: "None",
     path: "/",
-    secure: process.env.NODE_ENV === "production",
+    maxAge: 1000 * 60 * 60 * 24 * 5,
   });
 
   user.password = undefined;
-
-  return res.status(statusCode).json({ status: "success", token, user });
-};
+  return res.status(statusCode).json({ status: "success", user });
+}
